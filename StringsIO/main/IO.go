d58939878c960f2912e6c9009d54af8790dd66a8
AlesandroMarques <alesandro.marques@ibm.com>
2017-07-14 14:23:01 -0400

    go rountines added

--- StringsIO/main/IO.go
+++ StringsIO/main/IO.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"sort"
+	"os"
+	"log"
+	"io/ioutil"
+
+
+)
 
 func main()  {
-	fmt.Println("sup")
+
+	// String stuff
+	sampString:= "Hello World"
+	// returns true if phrase exists in string
+	fmt.Println(strings.Contains(sampString, "lo"))
+
+	// Returns the index for the match
+	fmt.Println(strings.Index(sampString, "lo"))
+
+	// Returns the number of matches for the string
+	fmt.Println(strings.Count(sampString, "l"))
+
+	// Replaces the first letter with the second as many times as you define
+	fmt.Println(strings.Replace(sampString, "l", "x", 3))
+
+	// Return a list seperating with defined seperator
+	csvString :="1,2,3,4,5,6"
+	fmt.Println(strings.Split(csvString, ","))
+	listOfStrings := strings.Split(csvString, ",")
+
+	for _, val := range listOfStrings{
+		fmt.Println(val)
+	}
+
+	//sort stuff
+	listOfLetters := []string{"a","c","b"}
+	sort.Strings(listOfLetters)
+	fmt.Println("Letters ", listOfLetters)
+
+	// Returns a string using the values passed in separated with separator
+	listOfNums := strings.Join([]string{"3","2","1"}, ",");
+	fmt.Println(listOfNums)
+
+
+
+	io()
+
+
+}
+
+
+func io(){
+	file , err := os.Create("samp.txt")
+
+	if err !=nil {
+
+		log.Fatal(err)
+	}
+	file.WriteString("hellloo")
+	file.Close()
+
+	stream , err := ioutil.ReadFile("samp.txt")
+	if err !=nil {
+		log.Fatal(err)
+
+	}
+
+	readString := string(stream)
+	fmt.Println(readString)
+
+
 }
