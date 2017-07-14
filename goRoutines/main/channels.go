8d64e5735fea0564c7e09e6a9cf0902e00639e1f
AlesandroMarques <alesandro.marques@ibm.com>
2017-07-14 14:57:26 -0400

    code added to goRoutines channels.go

--- goRoutines/main/channels.go
+++ goRoutines/main/channels.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var pizzaNum=0
+var pizzaName= ""
+
+func makeDough(stringChan chan string){
+	pizzaNum++;
+	pizzaName = "Pizza # "+ strconv.Itoa(pizzaNum)
+
+	fmt.Println("Make dough and send for sauce");
+
+	//send the pizzaName into channel for the next
+	stringChan <- pizzaName
+
+	time.Sleep(time.Millisecond*10)
+}
+
+func addSauce(stringChan chan string){
+pizza := <- stringChan
+	fmt.Println("Add sauce and send " , pizza , "for Toppings")
+
+	stringChan <-pizza
+	time.Sleep(time.Millisecond*10)
+}
+
+func addToppings(stringChan chan string){
+	pizza := <- stringChan
+	fmt.Println("add toppings to ", pizza, "and ship")
+
+	//time.Sleep(time.Millisecond*10)
+
+
+}
 
 func main()  {
-	fmt.Print("hello")
+stringChan := make(chan string)
+
+	for i:=0;i<3 ;i++  {
+		go makeDough(stringChan)
+		go addSauce(stringChan)
+		go addToppings(stringChan)
+
+		time.Sleep(time.Millisecond*5000)
+
+	}
 }
