b79776f66b710efc08cbdb24b1a4106463ce52f3
AlesandroMarques <alesandro.marques@ibm.com>
2017-07-14 14:37:48 -0400

    go channels added to routines

--- goRoutines/main/routine.go
+++ goRoutines/main/routine.go
@@ -1,6 +1,27 @@
 package main
 
-import "fmt"
+import ("fmt"
+"time"
+)
+
+func count(id int){
+	for i:=0;i<10 ;i++  {
+		fmt.Println(id , ":", i)
+
+		time.Sleep(time.Millisecond * 1000)
+	}
+
+}
+
 func main()  {
-	fmt.Println("rountines")
+	// A go routine is a function that runs in parallel with other functions
+	// We define one by using go followed by the function name
+
+	for i := 0; i < 10; i++ {
+		go count(i)
+	}
+
+	// Wait for the timer to make sure the go routine has time to
+	// finish otherwise the program would end before that happens
+	time.Sleep(time.Millisecond * 11000)
 }
